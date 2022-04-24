5e5a40b798b9f59b62485444180a1bdc61c16e0f
Jaehyeon Kim <dottami@gmail.com>
2022-04-24 11:59:48 +1000

    parameterize

--- tripdata.py
+++ tripdata.py
@@ -1,3 +1,4 @@
+import sys
 from pyspark.sql import SparkSession
 
 from utils import to_timestamp_df
@@ -6,8 +7,9 @@ if __name__ == "__main__":
     spark = SparkSession.builder.appName("Trip Data").getOrCreate()
 
     dbname = "tripdata"
-    tblname = "ny_taxi"
-    dest_path = "s3://emr-local-dev/tripdata/"
+    tblname = "ny_taxi" if len(sys.argv) <= 1 else sys.argv[1]
+    bucket_name = "emr-local-dev" if len(sys.argv) <= 2 else sys.argv[2]
+    dest_path = f"s3://{bucket_name}/{tblname}/"
     src_path = "s3://aws-data-analytics-workshops/shared_datasets/tripdata/"
     # read csv
     ny_taxi = spark.read.option("inferSchema", "true").option("header", "true").csv(src_path)
